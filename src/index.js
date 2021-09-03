21bc54e630d64317df78081da9cc2e1480183099
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-09-03 15:09:40 -0700

    tried making sync calls. Going to refactor search into a class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import PokeSearch from "./search";
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM loaded');
+  displayPokemon('?limit=5');
+});
--- src/search.js
+++ src/search.js
@@ -16,11 +16,40 @@ const addItemToList = function(item) {
   pokemonItem.innerHTML = item;
   pokemonItem.classList.add("pokemon-list-item");
   pokemonList.appendChild(pokemonItem);
-
 }
 
+const getPokemon = async function(query) {
+  console.log('in get')
+  let url = pokeApi + query;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Could not fetch pokemon');
+  }
+  const data = await response.json();
+  console.log('done with get');
+  return data
+}
 
-
+const displayPokemon = function(query="") {
+  console.log('In display')
+  pokemon = [];
+  getPokemon(query)
+    .then(data => {
+      pokemon.push(...data.results);
+      const html = pokemon.map(poke => {
+        return `
+          <li class="pokemon-list-item">
+            <span >${poke.name}</span>
+          </li>
+        `;
+      }).join('');
+      pokemonList.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Something wrong with fetch', error);
+    })
+  console.log('finished display')
+}
 
 
 const searchInput = document.querySelector(".search-input");
