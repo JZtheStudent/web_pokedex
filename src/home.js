2aadf4b1d2800aa2e5a82c0b68b631ae1443c6e4
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-09-08 09:29:50 -0700

    figured out how to hide a section on click

--- src/home.js
+++ src/home.js
@@ -121,3 +121,14 @@ window.addEventListener('DOMContentLoaded', () => {
  
 });
 
+let testButton = document.querySelector('.test-button');
+let mainPageContainer = document.querySelector('.main-page-container');
+testButton.onclick = function(e) {
+  e.preventDefault();
+  if (mainPageContainer.classList.contains('hidden')) {
+    mainPageContainer.classList.remove('hidden');
+  } else {
+    mainPageContainer.classList.add('hidden');
+  }
+  console.log(mainPageContainer.classList);
+}
